509d99637c719179099b2ee2a51ffbeb4d93e49a
santosandressa <andressasantosp0@gmail.com>
2023-07-16 18:16:10 -0300

    http get

--- src/main/java/com/crud/spring/Application.java
+++ src/main/java/com/crud/spring/Application.java
@@ -1,7 +1,11 @@
 package com.crud.spring;
 
+import com.crud.spring.model.Course;
+import com.crud.spring.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +14,15 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository){
+		return args -> {
+			courseRepository.deleteAll();
+			Course course = new Course();
+			course.setName("Angular");
+			course.setCategory("front-end");
+
+			courseRepository.save(course);
+		};
+	}
 }
---
+++ src/main/java/com/crud/spring/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.crud.spring.repository;
+
+import com.crud.spring.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
